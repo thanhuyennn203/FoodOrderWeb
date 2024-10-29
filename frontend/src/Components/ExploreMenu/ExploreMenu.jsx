32a6d73e8ee627b64995bce8cccc9cb45c68ebe8
Thanh Huyen <thanhhuyenbb4683@gmail.com>
2024-10-29 16:47:00 +0700

    complete show data directerly without MVC

--- frontend/src/Components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/Components/ExploreMenu/ExploreMenu.jsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./ExploreMenu.css";
-import { menu_list } from "../../assets/assets";
+//import { menu_list } from "../../assets/assets";
 
 const ExploreMenu = ({ category, setCategory }) => {
+  const [menu_list, setMenuList] = useState([]); // Khai báo state để lưu menu_list
+
+  useEffect(() => {
+    fetch("http://localhost:8801/category")
+      .then((res) => res.json())
+      .then((data) => {
+        setMenuList(data); // Cập nhật state với dữ liệu mới
+        console.log(data); // Hiển thị dữ liệu
+      })
+      .catch((err) => console.log(err));
+  }, []);
+  // console.log(menu_list);
   return (
     <div className="explore-menu" id="explore-menu">
       <h1>Explore our menu</h1>
@@ -12,6 +24,8 @@ const ExploreMenu = ({ category, setCategory }) => {
 
       <div className="menu-list">
         {menu_list.map((item, index) => {
+          console.log(item.thumnail);
+          const menuPath = item.thumnail + ".jpg";
           return (
             <div
               onClick={() =>
@@ -25,12 +39,12 @@ const ExploreMenu = ({ category, setCategory }) => {
               <div className="menu-img-container">
                 <img
                   className={category === item.menu_id ? "active" : ""}
-                  src={item.menu_image}
+                  src={menuPath}
                   alt=""
                 />
               </div>
 
-              <p className="menu-name">{item.menu_name}</p>
+              <p className="menu-name">{item.name}</p>
             </div>
           );
         })}
--- frontend/src/Components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/Components/FoodDisplay/FoodDisplay.jsx
@@ -1,12 +1,25 @@
-import React, { useContext } from "react";
-import { StoreContext } from "../../Context/StoreContext";
+import React, { useContext, useEffect, useState } from "react";
+//import { StoreContext } from "../../Context/StoreContext";
 import FoodItem from "../FoodItem/FoodItem";
 import "./FoodDisplay.css";
 import { FaBowlFood } from "react-icons/fa6";
 
 const FoodDisplay = ({ category }) => {
-  const { food_list } = useContext(StoreContext);
+  // const { food_list } = useContext(StoreContext);
   // console.log(food_list);
+
+  const [food_list, setFoodList] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8801/product")
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log(data);
+        setFoodList(data);
+      })
+      .catch((err) => console.log(err));
+  });
+
   return (
     <div className="food-display" id="food-display">
       <h2>
@@ -16,17 +29,18 @@ const FoodDisplay = ({ category }) => {
 
       <div className="food-display-list">
         {food_list.map((item, index) => {
-          if (category === "All" || item.category === category) {
-            // console.log(item.description);
+          if (category === "All" || item.category_id === category) {
+            const imgPath = "public/" + item.thumbnail + ".jpg";
+            // console.log(item.thumbnail);
             return (
               <FoodItem
                 key={index}
-                id={item._id}
+                id={item.product_id}
                 name={item.name}
                 price={item.price}
-                description={item.description}
+                //description={item.description}
                 rate={item.rate}
-                image={item.image}
+                image={imgPath}
               />
             );
           }
